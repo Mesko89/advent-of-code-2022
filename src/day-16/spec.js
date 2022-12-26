546abdd9cba1384e7b511fdede26205fc9c8f8a2
Dejan Mesar <dejan.mesar@devrev.ai>
2022-12-26 17:19:13 +0100

    remove day 16 part 2 unit test as it is not needed

--- src/day-16/spec.js
+++ src/day-16/spec.js
@@ -19,22 +19,4 @@ describe('Day 16: Proboscidea Volcanium', () => {
       ).toBe(1651);
     });
   });
-  describe('part2', () => {
-    it('should solve part 2', () => {
-      expect(
-        part2([
-          'Valve AA has flow rate=0; tunnels lead to valves DD, II, BB',
-          'Valve BB has flow rate=13; tunnels lead to valves CC, AA',
-          'Valve CC has flow rate=2; tunnels lead to valves DD, BB',
-          'Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE',
-          'Valve EE has flow rate=3; tunnels lead to valves FF, DD',
-          'Valve FF has flow rate=0; tunnels lead to valves EE, GG',
-          'Valve GG has flow rate=0; tunnels lead to valves FF, HH',
-          'Valve HH has flow rate=22; tunnel leads to valve GG',
-          'Valve II has flow rate=0; tunnels lead to valves AA, JJ',
-          'Valve JJ has flow rate=21; tunnel leads to valve II',
-        ])
-      ).toBe(1707);
-    });
-  });
 });
